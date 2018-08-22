3994ab9c36d9023e81177c684cde070b88256155
emersion <contact@emersion.fr>
2018-08-22 18:18:57 +0100

    Add ldso to gcore.sh

--- gcore.sh
+++ gcore.sh
@@ -11,7 +11,7 @@ cd $(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)
 
 root=$(pwd)
 
-for testcase in hello-static hello-dynamic coreutils-ls glibc-19818; do
+for testcase in hello-static hello-dynamic coreutils-ls glibc-19818 ldso; do
   if [ ! -d "$root/$testcase" ] ; then
     continue
   fi
@@ -22,12 +22,17 @@ for testcase in hello-static hello-dynamic coreutils-ls glibc-19818; do
     if [ ! -x "$exe" ]; then
       continue
     fi
+
+    main=main
+    if [ "$testcase" = ldso ] ; then
+      main=dl_main
+    fi
+
     export LD_BIND_NOW=1
+
     echo -e >&2 "${blue}Generating starti coredump for $testcase $exe${reset}"
     "$bindir/gcore-starti" "$exe"
-    echo -e >&2 "${blue}Generating main coredump for $testcase $exe${reset}"
-    "$bindir/gcore-at" main "$exe"
+    echo -e >&2 "${blue}Generating $main coredump for $testcase $exe${reset}"
+    "$bindir/gcore-at" "$main" "$exe"
   done
 done
-
-# TODO: add ld.so and GNU's ls
